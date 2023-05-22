89d483e6d4ce2a6c6f07b0d7fa3a219ea38b8703
William Foulks <wjfoulks@gmail.com>
2023-05-22 13:05:12 +0800

    Wrote skeleton form for new list html.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,9 +10,17 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
-    redirect_to list_path(@list)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entry
+    end
   end
+
+  def show
+
+  end
+
 
   private
 
