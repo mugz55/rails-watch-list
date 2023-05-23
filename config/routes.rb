9db81424e2860f40d9ebd01097819d73d67d0717
William Foulks <wjfoulks@gmail.com>
2023-05-23 11:00:42 +0800

    Fixed delete bug and moved hosted images into assets folder.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "lists#home"
   resources :lists, only: ['index', 'show', 'new', 'create'] do
-    resources :bookmarks, only: ['new', 'create', 'destroy']
+    resources :bookmarks, only: ['new', 'create']
   end
+  resources :bookmarks, only: ["destroy"]
 end
